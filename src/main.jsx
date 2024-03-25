1e8f2c8dc1b46e14b4e9fd0885a1d856c7eec14b
Sivasish Praharaj <sivasish48@gmail.com>
2024-03-25 18:46:22 +0530

    rtk config

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,6 @@ import App from './App.jsx'
 import './index.css'
 import { Provider } from 'react-redux'
 import store from './store/store.js'
-import { Footer,Header } from './components/index.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
