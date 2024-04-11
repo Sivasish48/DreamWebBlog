f97a7c1c58b8e7e70acb2c555311b831fada18de
Sivasish Praharaj <sivasish48@gmail.com>
2024-04-12 00:42:11 +0530

    global button components

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,22 @@
 import React from "react";
 
-function Button(){
+function Button({
+    // let us give sum parameters to the Button function
+    // These curly braces enclose the function's parameters, which are like customizable options you can pass to the function when you use it.
+    //children: This parameter allows you to specify what text or content will appear inside the button. It's like the label you want to write on the button.
+
+    children,
+    type='button',
+    bgColor='bg-blue-600',
+    textColor='text-white',
+    className='',
+    ...props
+    //...props: This special syntax allows you to pass in any additional properties (like onClick for handling clicks) that might be useful for the button's behavior. It's like a catch-all for any extra options you need.
+}){
     return(
-        <div>button</div>
+        <button className={`px-4 py-4 rounded-lg ${bgColor} ${textColor} ${className}`} {...props}>
+            {children}
+        </button>
     )
 }
 
